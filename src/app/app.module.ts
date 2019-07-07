99e09d0de3cea8b21f6401d19bffea3e82bfc722
Morteza Aghili <morteza.aghili90@gmail.com>
2019-07-08 01:10:20 +0430

    Made it responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
